6d361017e64870956b0b8ad5786d722d43933c52
Abrar Ul Haq <abrarulhaq4231@gmail.com>
2025-12-30 00:12:26 +0530

    Add files via upload

---
+++ kruskals_alg.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define V 4
+#define E 5
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Subset {
+    int parent;
+    int rank;
+};
+
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+}
+
+void Union(struct Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+int compare(const void* a, const void* b) {
+    struct Edge* e1 = (struct Edge*)a;
+    struct Edge* e2 = (struct Edge*)b;
+    return e1->weight - e2->weight;
+}
+
+void kruskalMST(struct Edge edges[]) {
+    struct Edge result[V];
+    int e = 0, i = 0;
+
+    qsort(edges, E, sizeof(edges[0]), compare);
+
+    struct Subset subsets[V];
+    for (int v = 0; v < V; v++) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (e < V - 1 && i < E) {
+        struct Edge next = edges[i++];
+
+        int x = find(subsets, next.src);
+        int y = find(subsets, next.dest);
+
+        if (x != y) {
+            result[e++] = next;
+            Union(subsets, x, y);
+        }
+    }
+
+    printf("Edge \tWeight\n");
+    for (i = 0; i < e; i++)
+        printf("%d - %d \t%d\n", result[i].src, result[i].dest, result[i].weight);
+}
+
+int main() {
+    struct Edge edges[E] = {
+        {0, 1, 10},
+        {0, 2, 6},
+        {0, 3, 5},
+        {1, 3, 15},
+        {2, 3, 4}
+    };
+
+    kruskalMST(edges);
+    return 0;
+}
